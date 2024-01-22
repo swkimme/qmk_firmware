1c769ec34077b8a10719f9a9a1875168d0effdc4
Sangwoo Kim <sangwookim.me@gmail.com>
2024-01-23 04:26:16 +0900

    x2

--- keyboards/rgoulter/x_2/config.h
+++ keyboards/rgoulter/x_2/config.h
@@ -15,25 +15,4 @@
  */
 #pragma once
 
-// RGB Lighting
-#ifdef RGBLIGHT_ENABLE
-#define DRIVER_LED_TOTAL 4
-#define RGBLED_NUM 4
-#define RGBLIGHT_LIMIT_VAL 80
-#define RGBLIGHT_DEFAULT_VAL 30
-#define RGBLIGHT_VAL_STEP 8
-#endif
-
-#ifdef RGB_MATRIX_ENABLE
-#define DRIVER_LED_TOTAL 70
-#define RGBLED_NUM 70
-#define RGB_MATRIX_LED_COUNT 70
-#define RGB_MATRIX_KEYPRESSES
-#endif
-
-#ifdef RGB_MATRIX_ENABLE
-#ifdef RGBLIGHT_ENABLE
-#error "rgblight and rgb_matrix both enabled"
-#endif
-#endif
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
